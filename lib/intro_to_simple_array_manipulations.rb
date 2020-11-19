38106c79970176a53b3059fd3e1e4195f058843b
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-19 22:42:03 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_concat(a,b)
+  a.concat(b)
+end
+
+def using_insert(a,element)
+  a.insert(4, element)
+end
+
+def using_uniq(a)
+  a.uniq
+end
+
+def using_flatten(a)
+  a.flatten
+end
+
+def using_delete_at(a,b)
+  a.delete_at(b)
+end
+
+
+def using_delete(a,b)
+  a.delete(b)
+end
+
